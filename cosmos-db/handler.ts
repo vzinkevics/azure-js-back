c7d5ed75a699d700fce855ae7312090cc1c09210
v.zinkevics <vadims.zinkevics@livewire.com>
2024-02-12 17:28:05 +0200

    task 6

--- cosmos-db/handler.ts
+++ cosmos-db/handler.ts
@@ -3,14 +3,9 @@ import { CosmosClient } from "@azure/cosmos";
 
 const credential = new DefaultAzureCredential();
 
-// const client = new CosmosClient({
-//     endpoint: "https://cos-app-sand-ne-889.documents.azure.com:443/",
-//     aadCredentials: credential
-// });
-
 const client = new CosmosClient({
     endpoint: "https://cos-app-sand-ne-889.documents.azure.com:443/",
-    key: "NXt3O68gZBoWx94mpVNvHggK0aYARuLjmJzPuUbg8cykRqUI0tU00IAM0pwZNUOZxvlqV6q6DB8fACDbj15X4g=="
+    aadCredentials: credential
 });
 
 const database = client.database('products-db');
